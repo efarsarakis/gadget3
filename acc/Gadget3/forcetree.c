2d65cca5b7bc6ebedf62a0d821d70ab8fda947d4
manos_epcc <manos_epcc@epcc-head.(none)>
2014-07-27 22:41:28 +0100

    Tried acc routine flag for shortrange but no luck.
    Will need to investigate more. First will simplify shortrange so I can work on it. Error given so far with acc flags/directives are:
    
    mpicc         -fastsse -ta=nvidia,kepler -acc -Minfo=accel -Mipa=inline    -I/shared/apps/gadget/libraries/gsl-gcc/include/ -I/shared/apps/gadget/libraries/fftw2-mvapichPGI/include/     -c -o gravtree.o gravtree.c
    PGC-S-0155-Accelerator region ignored; see -Minfo messages  (gravtree.c: 1490)
    gravity_primary_loop:
       1490, Accelerator region ignored
       1491, Accelerator restriction: function/procedure calls are not supported
             Accelerator restriction: loop has multiple exits
       1559, Accelerator restriction: unsupported call to 'force_treeevaluate_shortrange'
    PGC/x86-64 Linux 14.4-0: compilation completed with severe errors
    make: *** [gravtree.o] Error 2
    
    mpicc -fastsse -ta=nvidia,kepler -acc -Minfo=accel -Mipa=inline -I/shared/apps/gadget/libraries/gsl-gcc/include/ -I/shared/apps/gadget/libraries/fftw2-mvapichPGI/include/     -c -o forcetree.o forcetree.c
    PGC-S-0155-Kernel region ignored; see -Minfo messages  (forcetree.c)
    force_treeevaluate_shortrange:
       2590, Accelerator restriction: loop has multiple exits
       2592, Accelerator restriction: complex branching behavior was found
             Accelerator restriction: loop has multiple exits
    PGC/x86-64 Linux 14.4-0: compilation completed with severe errors

--- acc/Gadget3/forcetree.c
+++ acc/Gadget3/forcetree.c
@@ -2434,6 +2434,7 @@ int force_treeevaluate(int target, int mode, int *exportflag, int *exportnodecou
  *  memory-access panelty (which reduces cache performance) incurred by the
  *  table.
  */
+#pragma acc routine
 int force_treeevaluate_shortrange(int target, int mode, int *exportflag, int *exportnodecount,
 				  int *exportindex)
 {
