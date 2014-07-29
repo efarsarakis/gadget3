f0c1d3cdae86fbd73bfa68b7e15b6f8e418b328b
Farsarakis <s1361615@sms.ed.ac.uk>
2014-07-29 12:47:03 +0100

    Seems to be working like this...

--- acc/Gadget3/gravtree.c
+++ acc/Gadget3/gravtree.c
@@ -1512,13 +1512,13 @@ void gravity_tree(void)
 
 
 
-#pragma acc data copyin(BufferFullFlag, P, All) create(m_acc_x)
+#pragma acc data copyin(BufferFullFlag, P, All) create(m_acc_x, m_acc_y, m_acc_z)
 			//create(m_acc_x, m_no, m_exitFlag, \
 //		m_nodesinlist, m_ptype)
 			{
 
 
-#pragma acc kernels loop private(m_index, exitFlag, m_acc_x)
+#pragma acc kernels loop private(m_index, exitFlag, m_acc_x,m_acc_y, m_acc_z)
 			for (m_index=0; m_index<m_num_active_part; m_index++) //manos
 			{
 
