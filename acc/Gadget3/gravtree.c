b39bff231bded1fbc5a47111b6f09fe60e015980
Farsarakis <s1361615@sms.ed.ac.uk>
2014-07-29 12:03:07 +0100

    ignore

--- acc/Gadget3/gravtree.c
+++ acc/Gadget3/gravtree.c
@@ -1486,7 +1486,7 @@ void gravity_tree(void)
 			MyLongDouble m_acc_x;
 			int m_no;
 			int m_nodesinlist;
-			int m_exitFlag;
+			int m_exitFlag, m_ptype;
 
 #pragma acc data copy(BufferFullFlag, P, All) create(m_acc_x, m_no, m_exitFlag, \
 		m_nodesinlist, m_ptype)
@@ -1534,7 +1534,7 @@ void gravity_tree(void)
 
 						{
 											struct NODE *m_nop = 0;
-											int m_ptype, m_ninteractions, m_nexp, m_tabindex, m_task, m_listindex = 0;
+											int m_ninteractions, m_nexp, m_tabindex, m_task, m_listindex = 0;
 											double m_r2, m_dx, m_dy, m_dz, m_mass, m_r, m_fac, m_u, m_h, m_h_inv, m_h3_inv;
 											double m_dxx, m_dyy, m_dzz, m_pdxx, m_pdyy, m_pdzz;
 											double m_pos_x, m_pos_y, m_pos_z, m_aold;
