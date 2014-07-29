b9da070cb28e7cb17b6e6a8a764ef21011893e4a
Farsarakis <s1361615@sms.ed.ac.uk>
2014-07-29 19:43:13 +0100

    ignore

--- acc/Gadget3/gravtree.c
+++ acc/Gadget3/gravtree.c
@@ -1487,7 +1487,7 @@ void gravity_tree(void)
 #pragma acc data copy(BufferFullFlag, P, All, m_break) create(m_acc_x){
 
 //#pragma acc parallel loop copy(ProcessedFlag[0:All.MaxPart])
-#pragma acc parallel loop private(m_index, m_acc_x,m_acc_y, m_acc_z)
+#pragma acc parallel loop private(m_index, m_acc_x)
 			for (m_index=0; m_index<m_num_active_part; m_index++) //manos
 			{
 				int exitFlag = 0;
